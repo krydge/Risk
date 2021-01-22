07d2e732f2570d2bcb373aa5db9101ab13b68bcf
Andy Orton <andyorton13@gmail.com>
2021-01-22 12:21:47 -0700

    Inherited from IPlayer need to change RiskHub to use Name and token

--- Risk.Server/Player.cs
+++ Risk.Server/Player.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Risk.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Risk.Server
 {
-    public class Player
+    public class Player : IPlayer
     {
-        public string Username { get; set; }
         public string ConnectionId { get; set; }
+
+        public string Name => throw new NotImplementedException();
+
+        public string Token => throw new NotImplementedException();
+
+        public List<int> TerritoryCards { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
