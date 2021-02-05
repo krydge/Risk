4b538a3d1142435c268134e222fcb1ad9eb887af
Andy Orton <andyorton13@gmail.com>
2021-02-05 10:42:40 -0700

    fixed subdirectory error

--- GenerateContestantZip.ps1
+++ GenerateContestantZip.ps1
@@ -1,28 +1,25 @@
-# Script to build zip for any contestants
 param(
 	[int]$version,
 	[string]$outdir = "../risk$version"
 )
-#makes new directory in given out dir
 New-Item -Path $outdir -ItemType "directory"
+New-Item -Path $outdir\Shared -ItemType "directory"
+New-Item -Path $outdir\ConsoleClient -ItemType "directory"
 
 write-host "this is version $version"
-#command to build single file exe
 cd Risk.Server
 dotnet publish -c Release -o publish -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -r win-x64
 
-copying files over
 copy-item -path "publish/Risk.Server.exe" -destination "../$outdir"
 copy-item -path "publish/appsettings.json" -destination "../$outdir"
 
-deleting publish folder
 rm publish -r
 
 cd ..
-copy-item -path "Risk.Shared\*" -destination "..\risk$version"
-copy-item -path "Risk.Signalr.ConsoleClient\*" -destination "..\risk$version"
 
-#getting console client
+copy-item -path "Risk.Shared\*" -destination "..\risk$version\Shared"
+copy-item -path "Risk.Signalr.ConsoleClient\*" -destination "..\risk$version\ConsoleClient"
+
 cd risk.signalr.consoleclient
 dotnet publish -c Release -o publish -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -r win-x64
 
