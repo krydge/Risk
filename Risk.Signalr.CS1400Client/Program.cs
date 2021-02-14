7081711b54255153e5e2d057e14e976006c91b60
Adam Teichert <adam.teichert@snow.edu>
2021-02-13 22:55:56 -0700

    added C# sample client that basically only uses things discussed in my CS1400 class

---
+++ Risk.Signalr.CS1400Client/Program.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Risk.Shared;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Risk.Signalr.CS1400Client
+{
+    class Program
+    {
+        static HubConnection hubConnection;
+        static string officialName;
+        static (string[,] cellOwners, int[,] cellArmies) ToArrays(IEnumerable<BoardTerritory> cells) {
+            var cellList = cells.ToList();
+            int numRows = cells.Max(cell => cell.Location.Row) + 1;
+            int numCols = cells.Max(cell => cell.Location.Row) + 1;
+            string[,] cellOwners = new string[numRows, numCols];
+            int[,] cellArmies = new int[numRows, numCols];
+            foreach (var cell in cells) {
+                cellOwners[cell.Location.Row, cell.Location.Column] = cell.OwnerName;
+                cellArmies[cell.Location.Row, cell.Location.Column] = cell.Armies;
+            }
+            return (cellOwners, cellArmies);
+        }
+
+        static async Task Main(string[] args)
+        {
+            var playerName = "CS1400 Client";
+            var serverAddress = "http://localhost:5000";
+            if(args.Length == 1)
+            {
+                serverAddress = args[0];
+            }
+            Console.WriteLine($"Talking to the server at {serverAddress}.");
+
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl($"{serverAddress}/riskhub")
+                .Build();
+            hubConnection.On<IEnumerable<BoardTerritory>>(MessageTypes.YourTurnToDeploy, async (board) =>
+            {
+                int row, column;
+                (var owners, var armies) = ToArrays(board);
+                PlayerLogic.WhereDoYouWantToDeploy(
+                    officialName, owners, armies, out row, out column);
+                await DeployAsync(new Location(row, column));
+            });
+            hubConnection.On<IEnumerable<BoardTerritory>>(MessageTypes.YourTurnToAttack, async (board) =>
+            {
+                (var owners, var armies) = ToArrays(board);
+                if (PlayerLogic.DoYouWantToAttack(officialName, owners, armies)) {
+                    int fromRow, fromColumn;
+                    int toRow, toColumn;
+                    PlayerLogic.WhatAttackDoYouWantToDo(
+                        officialName, owners, armies,
+                        out fromRow, out fromColumn,
+                        out toRow, out toColumn);
+                    await AttackAsync(new Location(fromRow, fromColumn), new Location(toRow, toColumn));
+                } else {
+                    await AttackCompleteAsync();
+                }
+            });
+            hubConnection.On<string>(MessageTypes.JoinConfirmation, validatedName => {
+                officialName = validatedName;
+                Console.Title = validatedName;
+                Console.WriteLine($"Successfully joined server. Assigned Name is {validatedName}");
+            });
+
+            await hubConnection.StartAsync();
+            Console.WriteLine("My connection id is " + hubConnection.ConnectionId);
+            await SignupAsync(playerName);
+            // just wait ....
+            Console.ReadLine();
+        }
+
+        static async Task SignupAsync(string playerName)
+        {
+            await hubConnection.SendAsync(MessageTypes.Signup, playerName);
+        }
+        static async Task DeployAsync(Location desiredLocation) => await hubConnection.SendAsync(MessageTypes.DeployRequest, desiredLocation);
+        static async Task AttackAsync(Location from, Location to) => await hubConnection.SendAsync(MessageTypes.AttackRequest, from, to);
+        static async Task AttackCompleteAsync() => await hubConnection.SendAsync(MessageTypes.AttackComplete);
+    }
+}
---
+++ Risk.Signalr.CS1400Client/README.md
@@ -0,0 +1,9 @@
+# CS 1400 Sample Risk Client
+
+1. Change the methods in `PlayerLogic.cs`.
+2. In one terminal, run `dotnet run` from the `Risk.Server` folder.
+3. In another terminal, run `dotnet run` from this folder.
+4. Open the visualizer with a browser.  By default, server is running at [`http://localhost:5000`](http://localhost:5000).
+5. Start the game.
+
+
