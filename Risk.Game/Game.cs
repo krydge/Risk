b85254f8115f5219e5937ebbcb8459a6674ae3de
Adam Teichert <adam.teichert@snow.edu>
2021-02-22 22:57:22 -0700

    fixing bug in PlayerCanAttack: before would say false if the player had any groups of 2 or more armies that were surronded by only owned territories

--- Risk.Game/Game.cs
+++ Risk.Game/Game.cs
@@ -179,7 +179,10 @@ namespace Risk.Game
             foreach (var territory in Board.Territories.Where(t => t.Owner == player && EnoughArmiesToAttack(t)))
             {
                 var neighbors = Board.GetNeighbors(territory);
-                return neighbors.Any(n => n.Owner != player);
+                if (neighbors.Any(n => n.Owner != player))
+                {
+                    return true;
+                }
             }
             return false;
         }
