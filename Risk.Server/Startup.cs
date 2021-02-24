f102ffcaf5e0348e4410ae6229daef64f4395678
Jonathan Allen <jallen@snow.edu>
2021-02-23 17:06:03 -0700

    Update workflow

--- Risk.Server/Startup.cs
+++ Risk.Server/Startup.cs
@@ -25,7 +25,11 @@ namespace Risk.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddServerSideBlazor();
+            services.AddServerSideBlazor()
+                .AddCircuitOptions(o => 
+                {
+                    o.DetailedErrors = true;
+                });
             services.AddRazorPages();
             services.AddSignalR();
             services.AddCors(options =>
